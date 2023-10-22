9b961f045264ccf674dca84a925b71145f2cf380
ashpreetsingh72112 <69754940+ashpreetsinghanand@users.noreply.github.com>
2023-10-22 14:24:50 +0530

    shadcn added themes added

---
+++ form-builder/app/(dashboard)/page.tsx
@@ -0,0 +1,12 @@
+import Logo from '@/components/Logo'
+import ThemeSwitcher from '@/components/ThemeSwitcher'
+import { UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
+     Hello
+    </div>
+  )
+}
